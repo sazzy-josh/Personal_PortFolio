d49a9602f0d78924de5c828570c0614ed2c6ff84
Idahosa Joshua <idahosajoshua61@gmail.com>
2022-03-30 20:48:46 +0100

    create new overlay

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const menu = document.getElementById('bars')
-
+const overlay = document.getElementById('menuoverlay')
+const items = document.getElementsByTagName('li')
 function toggleNav() {
     menu.classList.toggle('change')
-    
+    overlay.classList.toggle('slide-out')
 }
 
 menu.addEventListener('click', toggleNav)
+
+let list = Array.from(items)
+list.forEach((item) => {
+    let child = item.firstElementChild
+    if(child.classList.contains('link')){
+        child.addEventListener('click',toggleNav)
+    }
+  }
+)
